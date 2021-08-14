59021b82bd483988b53e942d396d726836178224
MuhammadFadll <65841599+fadelun@users.noreply.github.com>
2021-08-14 09:04:37 +0700

    change variable and some things need to be fixed

--- js/divide.js
+++ js/divide.js
@@ -6,32 +6,42 @@ const results = document.querySelector('.answers').children;
 const generate = () => {
     const a = Math.floor(Math.random() * 5) + 1;
     const b = Math.floor(Math.random() * 10) + 1;
-    let c = eval(a / b).toFixed(2);
-    let fixedNum = c;
+    let answer = eval(a / b);
+    
 
     num1.innerHTML = a;
     num2.innerHTML = b;
+    if(Number.isInteger(answer) == false){
+        answer = answer.toFixed(1)
+    }
+    console.log(answer)
 
     // first logic
-    if(fixedNum){
+    if(answer){
         let choises = Math.floor(Math.random() * 3);
         [...results].forEach((item, i) => {
             if(choises == i){
-                item.innerHTML = fixedNum;
-                if(fixedNum == 0) {
+                item.innerHTML = answer;
+                if(answer == 0) {
                     item.innerHTML = 0;
                 }
             }else{
                 let wrongAnswer = Math.floor(Math.random() * 5) / Math.floor(Math.random() * 10);
-                if(fixedNum != wrongAnswer){
-                    item.innerHTML = wrongAnswer.toFixed(2);
+                if(answer != wrongAnswer){
+                    if(Number.isInteger(wrongAnswer) == false){
+                        item.innerHTML = wrongAnswer.toFixed(1);
+                        console.log(wrongAnswer)
+                    }else{
+                        item.innerHTML = wrongAnswer;
+                        console.log(wrongAnswer)
+                    }
                 }
             }
         })
 
         for(let i =0; i < results.length;i++){
             results[i].addEventListener('click', () => {
-                if(results[i].innerHTML == fixedNum){
+                if(results[i].innerHTML == answer){
                     generate()
                 }
             })
